ccc6edd76dd0aa02834d27c2c413aa0a3c2d4985
Connect2waqas <waqasnadan972@gmail.com>
2025-12-15 00:11:33 +0500

    Refactor Student class and add Car class implementation

--- intro.py
+++ intro.py
@@ -1,9 +1,21 @@
-import json
 class Student():
-    name = "waqas"
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    
+s1= Student("waqas",87)
+# print(s1.name,s1.marks)
 
+class Car():
+    # constructor function or constructor Method
+    def __init__(self,name, car_name):
+        self.brand_name = name
+        self.car_name = car_name
+    
+    def getName(self):
+        return f"{self.brand_name},\n {self.car_name}"
 
-s1 = Student()
-name = s1.name.replace("waqas","bashir")
-# print(s1.name.replace("waqas","Bashir"))
-print(name)
+car1 = Car("toyata","civic")
+car2 = Car("BMW","HOnda")
+
+print(car1.getName())
