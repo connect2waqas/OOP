771bd3e2220affd4eebf020e0abd46881c79df5f
Connect2waqas <waqasnadan972@gmail.com>
2026-01-01 19:35:34 +0500

    updating some data

--- intro.py
+++ intro.py
@@ -1,29 +1,32 @@
-class Student():
-    def __init__(self,name,marks):
+class CMS():
+    University_name = "UOH"
+    def __init__(self,name, reg_no, semester):
         self.name = name
-        self.marks = marks
+        self.reg_no = reg_no
+        self.semester = semester
+
     
-    @staticmethod
-    def hello():
-        print("Welcome!")
+    def get_Enroll(self):
+        return f"""
+        Student Name: {self.name}
+        Student Roll No : {self.reg_no}
+        Student semester: {self.semester}"""
 
-    def inner(self):
-        print("We are calling the staticmethod")
-        print(self.hello())
-s1= Student("waqas",87)
-print(s1.inner())
-# print(s1.name,s1.marks)
-
-class Car():
-    # constructor function or constructor Method
-    def __init__(self,name, car_name):
-        self.brand_name = name
-        self.car_name = car_name
     
-    def getName(self):
-        return f"{self.brand_name},\n {self.car_name}"
+    def upgrade_semester(self,new_semester):
+        self.semester = new_semester
+        return f"Your are promoted to semester: {self.semester}"
+    @classmethod
+    def welcome(cls):
+        return f"Welcome to {CMS.University_name}"
+    
 
-car1 = Car("toyata","civic")
-car2 = Car("BMW","HOnda")
+name = input("Enter name: ")
+reg_no = int(input("Enter Roll No: "))
+semester = int(input("Enter semseter NO: "))
+student_1 = CMS(name,reg_no,semester,)
+print(student_1.welcome())
+print(student_1.get_Enroll())
+new_semester = input("Enter semester: ")
+print(student_1.upgrade_semester(new_semester))
 
-print(car1.getName())
