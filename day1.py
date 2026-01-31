1a3ce4108b72d4ef4e06cc292317cbc228f0c76d
Connect2waqas <waqasnadan972@gmail.com>
2026-02-01 00:12:00 +0500

    refactor Person and Student classes for improved display and grading functionality

--- day1.py
+++ day1.py
@@ -1,13 +1,12 @@
 class Person:
+    person_details = {}
     def __init__(self,name , age, address):
         self.name = name
         self.age = age
         self.address = address
     def display(self):
-        print(self.name)
-        print(self.age)
-        print(self.address)
-
+        ls = [self.name,self.age,self.address]
+        return ls
 class Student(Person):
     def __init__(self, name, age, address,roll_no,marks,):
         super().__init__(name, age, address)
@@ -16,16 +15,34 @@ class Student(Person):
         # self.grade = grade
     def grade(self):
         if 90 < self.marks <= 100:
-            return "You got grade A"
+            return "A"
         elif 80 < self.marks <= 90:
-            return "You got grade B"
+            return "B"
         elif 70 < self.marks <= 80:
-            return "You got grade C"
+            return "C"
         else:
-            return "You got grade D"
+            return "D"
     def display_info(self):
         grad = self.grade()
-        return f"The {self.name} has got {grad}"
+        return f"Name: {self.name}, Roll no: {self.roll_no}, Grade: {grad}"
+class Teacher(Person):
+    def __init__(self, name, age, address, employee_id, subject, salary):
+        super().__init__(name, age, address)
+        self.employee_id = employee_id
+        self.subject = subject
+        self.salary = salary
+    def give_assignment():
+        print("this is your assignments")
 
+    def display(self):
+        employee_info = super().display()[:2] + [self.address, self.subject, self.salary]
+        Person.person_details[self.employee_id] = employee_info
+        return employee_info
+    
+    
+    
 student1 = Student("waqas",21,"Talash",24,90)
+teacher1 = Teacher("Ilyas",19,"Talash",3234,"Electronic Engineer",90000)
 print(student1.display_info())
+print(teacher1.display())
+print(Person.person_details)
