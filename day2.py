ecfbd5e7849c503790cde5307edc63548254d4a0
Connect2waqas <waqasnadan972@gmail.com>
2026-02-01 02:33:13 +0500

    add SavingAccount class with interest calculation and withdrawal validation

--- day2.py
+++ day2.py
@@ -19,8 +19,34 @@ class BankAccount:
     @classmethod
     def create_accounts(cls,account_holder,initial_deposit):
         account_number = random.randint(1000,5000)
+        cls.total_accounts +=1
         return cls(account_number,account_holder,initial_deposit)
-    
+    @classmethod
+    def get_total_accounts(cls):
+        return cls.total_accounts
+    @staticmethod
+    def is_valid_amount(amount):
+        return True if amount > 0 else False
+class SavingAccount(BankAccount):
+    minimum_balance = 1000
+    def __init__(self, account_number, account_holder, balance, interest_rate = 0.035):
+        super().__init__(account_number, account_holder, balance)
+        self.interest_rate = interest_rate
+    def add_interest(self,time):
+        interest = self.interest_rate * self.balance * time
+        self.balance += interest
+        return self.balance
+    def withdraw(self, amount):
+        current_amount = self.get_balance()
+        if current_amount < SavingAccount.min_balance:
+            print("Witdrawal denied due to Insuffienct balance")
+        else:
+            current_amount += amount
+        return current_amount
+
 acc1 = BankAccount(2342,"waqas khan",9000)
 acc2 = BankAccount.create_accounts("Ilyas",90000)
 print(acc2.account_holder,acc2.account_number,acc2.balance)
+saving1 = SavingAccount(2442,"Waqas khan",1000,)
+print(saving1.add_interest(4))
+
