28514d71a0104aae0b0d416d824b663e4822c823
Connect2waqas <waqasnadan972@gmail.com>
2026-03-19 00:47:06 +0500

    fix: remove unnecessary parentheses from Student class definition and add Car class with acceleration and braking methods

--- lab 5/basc.py
+++ lab 5/basc.py
@@ -1,4 +1,4 @@
-class Student():
+class Student:
     def __init__(self,name,age,roll_no):
         self.name = name
         self.age = age
---
+++ lab 5/car.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+    def accelarate(self, increase):
+        self.speed += increase
+        return self.speed
+    def brake(self, decrease):
+        self.speed -= decrease
+        return self.speed
+    def __str__(self):
+        return (f"Car : {self.brand}\n"
+                f"Speed: {self.speed}km\n"
+                f"Accalarate: {self.accelarate(59)}\n"
+                f"Brake: {self.brake(50)}")
+car_1 = Car("Toyota",200)
+print(car_1)
