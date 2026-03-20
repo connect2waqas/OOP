fdc12a7b6cd73b5e90a534a81ce347ed2cf51613
Connect2waqas <waqasnadan972@gmail.com>
2026-03-21 02:05:30 +0500

    fix: correct author assignment in Book class and refactor Library methods for better clarity

--- lab 5/library_system.py
+++ lab 5/library_system.py
@@ -1,36 +1,44 @@
 class Book:
     def __init__(self, title, auther):
         self.title = title
-        self.auther = auther
+        self.auther = title
+
 class Library:
     def __init__(self):
-        self.book = []
-        self.issued_book = []
-    def add_book(self, name):
-        if isinstance(name, list):
-            self.book.extend(name)
-        else:
-            self.book.append(name)
-        print(f"{self.book} is added to library")
-    def issue_book(self,book):
-        if book in self.issued_book:
-            print("book already issued")
-            return (self.issued_book, self.book)
-        if book not in self.book:
-            print("book is not available in library")
-            return (self.issued_book, self.book)
-        self.issued_book.append(book)
-        self.book.remove(book)
-        return (self.issued_book, self.book)
-    def return_book(self, book):
-        if book not in self.issued_book:
-            print("book was not issued")
-            return (self.issued_book, self.book)
-        self.issued_book.remove(book)
-        self.book.append(book)
-        return (self.issued_book, self.book)
+        self.all_book_with_authers = {}
+        self.all_books = []
+        self.authers = []
+        self.all_issued_books = []
+        # for i in range(len(self.all_books)):
+        #     self.all_book_with_authers[self.all_books[i]] = self.authers[i]
+        for book , auther in zip(self.all_books, self.authers):
+            self.all_book_with_authers[book] = auther
 
-ls_books = ["python basics","Ml basics", "Deep learning basics"]
-li = Library()
-li.add_book(ls_books)
-print(li.issue_book("Ml basics"))
+    def adding_books(self,books, auther):
+        self.all_books.extend(books)
+        self.authers.extend(auther)
+        print(f"{books} is added!")
+        print("all auther is added")
+
+
+    def check_book_avilable(self, book, auther):
+       return True if self.all_book_with_authers.get(book) == auther else False
+    
+    def issuing_book(self, book, auther):
+        if self.check_book_avilable(book, auther):
+            print(f"{book} is avialable and issued now!")
+            self.all_issued_books.append(book)
+        else:
+            print(f"{book} with such auther {auther} is not avialable")
+        return self.all_issued_books, self.all_books
+    def returning_book(self, book):
+        self.all_books.extend(book)
+b_1 = Book("Python basics","Waqas")
+b_2 = Book("ML basics","Waqas")
+
+l = Library()
+l.adding_books(["python","ml","dp","nlp"],["waqas","bashir","ilyas","abbas"])
+print(l.all_books)
+print(l.authers)
+print(l.all_book_with_authers)
+print(l.issuing_book("python","waqas"))
