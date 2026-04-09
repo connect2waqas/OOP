7b99eed037352da278f85d6c2ebf628e6a742fc5
Connect2waqas <waqasnadan972@gmail.com>
2026-04-10 01:55:06 +0500

    add: implement class hierarchy with method overriding and demonstrate method resolution

--- Inheritance/practice_1.py
+++ Inheritance/practice_1.py
@@ -118,3 +118,46 @@ class Child(Parent):
 
 son = Child()
 son.get_num()
+
+class A:
+
+    def m1(self):
+        return 20
+
+class B(A):
+
+    def m1(self):
+        return 30
+
+    def m2(self):
+        return 40
+
+class C(B):
+  
+    def m2(self):
+        return 20
+obj1=A()
+obj2=B()
+obj3=C()
+print(obj1.m1() + obj3.m1()+ obj3.m2())
+
+""" the following will give error due to infinite calling of the self.m1() method
+class A:
+
+    def m1(self):
+        return 20
+
+class B(A):
+
+    def m1(self):
+        val=super().m1()+30
+        return val
+
+class C(B):
+  
+    def m1(self):
+        val=self.m1()+20
+        return val
+obj=C()
+print(obj.m1())
+"""
